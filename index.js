069400f7c213b10d67f32af6a1d34806e474a8db
Helena Alinder <helenaalinder@gmail.com>
2019-11-04 16:37:14 +0100

    Add simple function to count requests

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-exports.testDostroyPackage = function() {
-  console.log("This is a message from the dostroy package");
+const fs = require('fs')
+
+const logSession = new Date().toISOString()
+const logStream = fs.createWriteStream('/tmp/express-requests-' + logSession + '.log', {flags:'a'})
+
+let _requests = 0
+
+module.exports = {
+  countRequests: function(req, res, next) {
+    _requests++
+    const now = new Date()
+    logStream.write('Connection requested at: ' + now + ', connection attempt: ' + _requests + '\n')
+    next()
+  },
+  getRequests: function() {
+    const requests = _requests
+    return requests
+  }
 }
