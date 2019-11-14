663eb74862711f6bdaa6e12660939292aa8da77b
Helena Alinder <helenaalinder@gmail.com>
2019-11-14 14:06:04 +0100

    Clean up code

--- index.js
+++ index.js
@@ -4,46 +4,49 @@ const moment = require('moment')
 const logSession = new Date().toISOString()
 const logStream = fs.createWriteStream('/tmp/express-requests-' + logSession + '.log', {flags:'a'})
 
-const interval = 3000 // milliseconds to check number of requests
-const limit = 3 // limit for requests within interval
-let _rateLimitingAddress2Requests = {}
+const _interval = 3000 // milliseconds to check number of requests
+const _limit = 3 // limit for requests within interval
+let _rlAddressToRequests = {}
 
 const formatMoment = (moment) => {
   return moment.format()
 }
 
-const addAddress2Requests = (address, requests, startRequestAt) => {
-  _rateLimitingAddress2Requests[address] = { requests: requests, startRequestAt: startRequestAt }
+const addAddressToRequests = (address, requests, startRequestAt) => {
+  _rlAddressToRequests[address] = { requests, startRequestAt }
+}
+
+const logRateLimiting = (moment, address, interval, requests, status) => {
+  logStream.write('[' + formatMoment(moment) + ']{ Address: ' + address + ', interval: ' + interval + 'ms, requests: ' + requests + ', status: ' + status + ' }\n')
 }
 
 const rateLimiting = (req, res, next) => {
   const address = req.connection.remoteAddress
   const now = moment()
-  const addressObject = _rateLimitingAddress2Requests[address]
+  const addressObject = _rlAddressToRequests[address]
   if (!addressObject) {
-    addAddress2Requests(address, 1, now)
+    addAddressToRequests(address, 1, now)
     return next()
   }
 
   let requests = addressObject.requests
-  let startRequestAt = addressObject.startRequestAt
-  let rateLimitingDiffSeconds = now.diff(startRequestAt)
-  if (rateLimitingDiffSeconds < interval && requests > limit) {
-    addAddress2Requests(address, requests + 1, now)
-    logStream.write('[' + formatMoment(_rateLimitingAddress2Requests[address].startRequestAt) + ']{ Address: ' + address + ', interval: ' + interval + 'ms, requests: ' + _rateLimitingAddress2Requests[address].requests + ', status: ended }\n')
+  const startRequestAt = addressObject.startRequestAt
+  const diffSeconds = now.diff(startRequestAt)
+  if (diffSeconds < _interval && requests > _limit) {
+    addAddressToRequests(address, requests + 1, now)
+    logRateLimiting(_rlAddressToRequests[address].startRequestAt, address, _interval, _rlAddressToRequests[address].requests, 'ended')
     return res.end()
-  } else if (rateLimitingDiffSeconds < interval) {
-    addAddress2Requests(address, requests + 1, startRequestAt)
+  } else if (diffSeconds < _interval) {
+    addAddressToRequests(address, requests + 1, startRequestAt)
   } else {
-    addAddress2Requests(address, 1, now)
+    addAddressToRequests(address, 1, now)
   }
-  logStream.write('[' + formatMoment(_rateLimitingAddress2Requests[address].startRequestAt) + ']{ Address: ' + address + ', interval: ' + interval + 'ms, requests: ' + _rateLimitingAddress2Requests[address].requests + ', status: ok }\n')
+  logRateLimiting(_rlAddressToRequests[address].startRequestAt, address, _interval, _rlAddressToRequests[address].requests, 'ok')
   return next()
 }
 
 const getAddresses = () => {
-  const addresses = _rateLimitingAddress2Requests
-  return addresses
+  return _rlAddressToRequests
 }
 
 module.exports = {
